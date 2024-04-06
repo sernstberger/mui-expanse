d0104ff35cdaa004f3ca9327d7288d406fd40147
Steve Ernstberger <steve32285@gmail.com>
2024-04-06 16:24:29 -0400

    added mui

--- apps/vacation-rental/src/app/app.tsx
+++ apps/vacation-rental/src/app/app.tsx
@@ -1,13 +1,29 @@
-import styled from '@emotion/styled';
-
-import NxWelcome from './nx-welcome';
-
-const StyledApp = styled.div`
-  // Your style here
-`;
+import { Button, Card, Grid } from '@mui/material';
 
 export function App() {
-  return <StyledApp>cool</StyledApp>;
+  return (
+    <div>
+      <header>header goes here</header>
+      <main>
+        <ProductGrid />
+        <Button>cool</Button>
+      </main>
+    </div>
+  );
 }
 
 export default App;
+
+const ProductGrid = () => {
+  return (
+    <Grid container spacing={2}>
+      {[1, 2, 3, 4, 5].map((item) => {
+        return (
+          <Grid item xs={3} key={item}>
+            <Card>card {item}</Card>
+          </Grid>
+        );
+      })}
+    </Grid>
+  );
+};
--- apps/vacation-rental/src/main.tsx
+++ apps/vacation-rental/src/main.tsx
@@ -1,13 +1,19 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 import App from './app/app';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const theme = createTheme({});
+
 root.render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </StrictMode>
 );
