16c31dc7872957a42253f8febe85765584703f18
Steve Ernstberger <steve32285@gmail.com>
2024-04-06 15:40:59 -0400

    initial commit

---
+++ apps/vacation-rental/src/app/app.tsx
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+
+import NxWelcome from './nx-welcome';
+
+const StyledApp = styled.div`
+  // Your style here
+`;
+
+export function App() {
+  return <StyledApp>cool</StyledApp>;
+}
+
+export default App;
