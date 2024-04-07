72db3d733e01fd1575dd87510264c40eb4c2be52
Steve Ernstberger <steve32285@gmail.com>
2024-04-07 09:10:09 -0400

    moved theme toggle

--- apps/vacation-rental/src/app/app.tsx
+++ apps/vacation-rental/src/app/app.tsx
@@ -6,62 +6,16 @@ import {
   createTheme,
   ThemeProvider,
   CssBaseline,
-  ToggleButtonGroup,
-  ToggleButton,
 } from '@mui/material';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import * as React from 'react';
 import { PaletteMode } from '@mui/material';
-import Box from '@mui/material/Box';
 
 import getLPTheme from './getLPTheme';
-import AutoAwesomeRoundedIcon from '@mui/icons-material/AutoAwesomeRounded';
 import { AppAppBar } from './AppAppBar';
-
-interface ToggleCustomThemeProps {
-  showCustomTheme: boolean;
-  toggleCustomTheme: () => void;
-}
-
-function ToggleCustomTheme({
-  showCustomTheme,
-  toggleCustomTheme,
-}: ToggleCustomThemeProps) {
-  return (
-    <Box
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        width: '100dvw',
-        position: 'fixed',
-        bottom: 24,
-      }}
-    >
-      <ToggleButtonGroup
-        color="primary"
-        exclusive
-        value={showCustomTheme}
-        onChange={toggleCustomTheme}
-        aria-label="Platform"
-        sx={{
-          backgroundColor: 'background.default',
-          '& .Mui-selected': {
-            pointerEvents: 'none',
-          },
-        }}
-      >
-        <ToggleButton value>
-          <AutoAwesomeRoundedIcon sx={{ fontSize: '20px', mr: 1 }} />
-          Custom theme
-        </ToggleButton>
-        <ToggleButton value={false}>Material Design 2</ToggleButton>
-      </ToggleButtonGroup>
-    </Box>
-  );
-}
+import { ToggleCustomTheme } from './ToggleCustomTheme';
 
 export function App() {
   const [mode, setMode] = React.useState<PaletteMode>('light');
